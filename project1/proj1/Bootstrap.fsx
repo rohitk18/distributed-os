82b90c60432d1467a8bcc3c2cb4f2623069e89b3
Rohit K <krohit.bphc@gmail.com>
2020-09-21 09:52:40 -0400

    added bootstrap file

---
+++ project1/proj1/Bootstrap.fsx
@@ -0,0 +1,6 @@
+
+
+#r @"bin/Debug/netcoreapp3.1/Akka.dll"
+#r @"bin/Debug/netcoreapp3.1/Akka.FSharp.dll"
+#r @"bin/Debug/netcoreapp3.1/Akka.Remote.dll"
+#r @"bin/Debug/netcoreapp3.1/Akka.TestKit.dll"
