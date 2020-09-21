afb9cecb72031fffb3bd3ea8237882427e102be0
Rohit K <krohit.bphc@gmail.com>
2020-09-21 09:52:05 -0400

    Working code, subproblem pending

--- project1/proj1/Program.fs
+++ project1/proj1/Program.fs
@@ -2,110 +2,90 @@
 open Akka.Actor
 open Akka.FSharp
 
-type Bint = Numerics.BigInteger
+type ResultObject = ResultObject of isquare:bool * startnum:bigint
 
-let sumOfSquares (n:int):int =
+let sumOfSquares (n:bigint):bigint =
     (* Sum of squares of positive integers from 1 to n (including) *)
-    match n with
-    | 0 -> 0
-    | _ -> n * (n+1) * (2*n + 1) / 6
+    if n.IsZero then 0I
+    else n * (n+1I) * (2I*n + 1I) / 6I
 
-let sumOfSquaresRange (n:int,k:int):int = 
+let checkSquare (n:bigint) =
+    (* Sum of squares of positive integers from 1 to n (including) *)
+    // printf "test: %A\n" n
+    let sqroot=sqrt (double n)
+    let number = truncate sqroot
+    number*number = (double)n 
+
+let sumOfSquaresRange (n:bigint,k:bigint):ResultObject = 
     (* Sum of squares of integers from n to n+k-1 *)
-    match k with
-    | 0 -> 0
-    | 1 -> sumOfSquares n
-    | _ -> sumOfSquares(n+k-1) - sumOfSquares(n-1)
-
-// let checkPerfectSquare (n:bigint):bool =
-//     (* Sum of squares of positive integers from 1 to n (including) *)
-//     let sqroot=sqrt (float n)
-//     let number = truncate sqroot
-//     number*number= (float)n 
-
+    if n.IsZero then ResultObject(false,0I)
+    elif n.IsOne then ResultObject(k |> sumOfSquares |> checkSquare,1I)
+    else ResultObject(sumOfSquares(n+k-1I) - sumOfSquares(n-1I) |> checkSquare,n)
 
 [<EntryPoint>]
 let main argv =
     // get arguments from command line as big integers
-    let n = int argv.[0]
-    let k = int argv.[1]
-    Console.WriteLine("{0}",sumOfSquaresRange(n,k))
+    let n:bigint = System.Numerics.BigInteger.Parse(argv.[0])
+    let k:bigint = System.Numerics.BigInteger.Parse(argv.[1])
+    // Console.WriteLine("{0}",sumOfSquaresRange(n,k))
 
     let rootSystem = ActorSystem.Create("Proj1")
-    (*
-    // let masterActor = 
-    //     spawn rootSystem "master-actor"
-    //     <| fun mailbox ->
-    //         actor {
-    //             let! message = mailbox.Receive()
-    //             match box message with
-    //             | :? string as msg -> printfn "Hello %s" msg
-    //             | _ ->  failwith "unknown message"
-    //         } 
-    
-    // masterActor <! "F#!"
-    *)
 
     let masterActor (calculators:List<_>) (mailbox:Actor<_>) = 
         let rec loop() = actor {
             let! message = mailbox.Receive()
-            match box message with
-            // | :? string as msg -> printfn "ScalarValuePrinter: received String %s" msg
-            | :? string as msg -> 
+            match message with
+            | "Start" -> 
                 // for each calculator send message "Calculate" executes calculator actors
-                printfn "Message at master: %s" msg
-                calculators
-                |> List.map(fun calc -> 
-                                calc <! msg)
-            | :? int as msg -> printfn "ScalarValuePrinter: received Int %i" msg
-            | _ -> ()
+                // calculators
+                // |> List.map(fun calc -> 
+                //                 calc <! "Calculate")
+                for calc in calculators do
+                    calc <! "Calculate"
+            | _ -> failwith "Improper Command"
             return! loop()
         }
         loop()
 
-    let calculator receiverActor num (mailbox:Actor<_>) =
-        let rec loop ractor n = actor {
+    let calculator receiverActor (num:bigint) (mailbox:Actor<_>) =
+        let rec loop ractor no = actor {
             let! message = mailbox.Receive()
-            match box message with
-            | :? string as msg -> 
-                printfn "Message:%s Calculating %i...Done" msg n
-                ractor <! msg
-            | _->()
-            return! loop ractor n
+            match message with
+            | "Calculate" -> 
+                // printfn "Calculating %A...Done" no
+                ractor <! sumOfSquaresRange(no, k)
+            | _-> failwith "Improper Calculator Command"
+            return! loop ractor no
         }
         loop receiverActor num
     
-    let receiver num (mailbox:Actor<_>) =
-        let rec loop n = actor{
-            let! message = mailbox.Receive()
-            match box message with
-            | :? string as msg -> printfn "Message at Receiver: %s %i" msg n
-            | _->()
-            return! loop n
+    let receiver (num:bigint) (mailbox:Actor<_>) =
+        let mutable x = num
+        let rec loop() = actor{
+            let! ResultObject(b,s) = mailbox.Receive()
+            if x > 0I then
+                match b with
+                | true -> printfn "%A" s
+                | false -> ()
+                x <- x - 1I
+            else
+                failwith "Result Overload"
+            return! loop()
         }
-        loop num
+        loop ()
 
-    // let maRef = spawn rootSystem "master-actor" masterActor
     let maReceiver = spawn rootSystem "receiver-actor" <| receiver n
 
-
-    // let maCalculators = spawn rootSystem "ma-calculators" <| calculator maRef n
-    // let maCalculators = spawn rootSystem "ma-calculators" <| calculator maReceiver n
-    // let mac1 = spawn rootSystem "ma-calculators-1" <| calculator maReceiver 1
-    // let mac2 = spawn rootSystem "ma-calculators-2" <| calculator maReceiver 2
     let maCalculators = 
-        [1 .. n]
+        [1I .. n]
         |> List.map(fun id ->   
-                        // printfn "%i" id
                         let sid = string id
                         let cs = "ma-calc-" + sid
-                        // printfn "%s" cs )
                         spawn rootSystem cs  <| calculator maReceiver id)
     
-    // let maRef = spawn rootSystem "master-actor" <| masterActor [mac1,mac2]
     let maRef = spawn rootSystem "master-actor" <| masterActor maCalculators
     
-    maRef <! "Knock knock!"
+    maRef <! "Start"
     printfn "End of code"
     Threading.Thread.Sleep 5000
     0 // return an integer exit code
---
+++ project1/proj1/proj1.fsx
@@ -0,0 +1,90 @@
+#time "on"
+#load "Bootstrap.fsx"
+
+open System
+open Akka.Actor
+open Akka.FSharp
+
+type ResultObject = ResultObject of isquare:bool * startnum:bigint
+
+let sumOfSquares (n:bigint):bigint =
+    (* Sum of squares of positive integers from 1 to n (including) *)
+    if n.IsZero then 0I
+    else n * (n+1I) * (2I*n + 1I) / 6I
+
+let checkSquare (n:bigint) =
+    (* Sum of squares of positive integers from 1 to n (including) *)
+    // printf "test: %A\n" n
+    let sqroot=sqrt (double n)
+    let number = truncate sqroot
+    number*number = (double)n 
+
+let sumOfSquaresRange (n:bigint,k:bigint):ResultObject = 
+    (* Sum of squares of integers from n to n+k-1 *)
+    if n.IsZero then ResultObject(false,0I)
+    elif n.IsOne then ResultObject(k |> sumOfSquares |> checkSquare,1I)
+    else ResultObject(sumOfSquares(n+k-1I) - sumOfSquares(n-1I) |> checkSquare,n)
+
+let n:bigint = System.Numerics.BigInteger.Parse fsi.CommandLineArgs.[1]
+let k:bigint = System.Numerics.BigInteger.Parse fsi.CommandLineArgs.[2]
+printfn "%A %A" n k
+
+let rootSystem = ActorSystem.Create("Proj1")
+
+let masterActor (calculators:List<_>) (mailbox:Actor<_>) = 
+    let rec loop() = actor {
+        let! message = mailbox.Receive()
+        match message with
+        | "Start" -> 
+            // for each calculator send message "Calculate" executes calculator actors
+            // calculators
+            // |> List.map(fun calc -> 
+            //                 calc <! "Calculate")
+            for calc in calculators do
+                calc <! "Calculate"
+        | _ -> failwith "Improper Command"
+        return! loop()
+    }
+    loop()
+
+let calculator receiverActor (num:bigint) (mailbox:Actor<_>) =
+    let rec loop ractor no = actor {
+        let! message = mailbox.Receive()
+        match message with
+        | "Calculate" -> 
+            // printfn "Calculating %A...Done" no
+            ractor <! sumOfSquaresRange(no, k)
+        | _-> failwith "Improper Calculator Command"
+        return! loop ractor no
+    }
+    loop receiverActor num
+    
+let receiver (num:bigint) (mailbox:Actor<_>) =
+    let mutable x = num
+    let rec loop() = actor{
+        let! ResultObject(b,s) = mailbox.Receive()
+        if x > 0I then
+            match b with
+            | true -> printfn "%A" s
+            | false -> ()
+            x <- x - 1I
+        else
+            failwith "Result Overload"
+        return! loop()
+    }
+    loop ()
+
+let maReceiver = spawn rootSystem "receiver-actor" <| receiver n
+
+let maCalculators = 
+    [1I .. n]
+    |> List.map(fun id ->   
+                    let sid = string id
+                    let cs = "ma-calc-" + sid
+                    spawn rootSystem cs  <| calculator maReceiver id)
+
+let maRef = spawn rootSystem "master-actor" <| masterActor maCalculators
+
+maRef <! "Start"
+
+// Threading.Thread.Sleep 5000
