0196508830e9cd1afbd758769e9d86548566e123
Rohit K <krohit.bphc@gmail.com>
2020-12-11 03:56:16 -0500

    REST implementation of a github project.
    Next: including twitter engine into the controller i.e. backend module. Also change names

--- project42/TwitterAPI/Site.fs
+++ project42/TwitterAPI/Site.fs
@@ -1,58 +1,194 @@
 namespace TwitterAPI
 
+open System
+open System.Collections.Generic
 open WebSharper
 open WebSharper.Sitelets
 open WebSharper.UI
 open WebSharper.UI.Server
 
-type EndPoint =
-    | [<EndPoint "/">] Home
-    | [<EndPoint "/about">] About
+/// The types used by this application.
+module Model =
 
-module Templating =
-    open WebSharper.UI.Html
+    /// Data about a person. Used both for storage and JSON parsing/writing.
+    type PersonData =
+        {
+            id: int
+            firstName: string
+            lastName: string
+            born: System.DateTime
+            /// Since this is an option, this field is only present in JSON for Some value.
+            died: option<System.DateTime>
+        }
 
-    type MainTemplate = Templating.Template<"Main.html">
+    /// The type of REST API endpoints.
+    /// This defines the set of requests accepted by our API.
+    type ApiEndPoint =
 
-    // Compute a menubar where the menu item for the given endpoint is active
-    let MenuBar (ctx: Context<EndPoint>) endpoint : Doc list =
-        let ( => ) txt act =
-             li [if endpoint = act then yield attr.``class`` "active"] [
-                a [attr.href (ctx.Link act)] [text txt]
-             ]
-        [
-            "Home" => EndPoint.Home
-            "About" => EndPoint.About
-        ]
+        /// Accepts GET requests to /people
+        | [<EndPoint "GET /people">]
+            GetPeople
 
-    let Main ctx action (title: string) (body: Doc list) =
-        Content.Page(
-            MainTemplate()
-                .Title(title)
-                .MenuBar(MenuBar ctx action)
-                .Body(body)
-                .Doc()
-        )
+        /// Accepts GET requests to /people/{id}
+        | [<EndPoint "GET /people">]
+            GetPerson of id: int
 
+        /// Accepts POST requests to /people with PersonData as JSON body
+        | [<EndPoint "POST /people"; Json "personData">]
+            CreatePerson of personData: PersonData
+
+        /// Accepts PUT requests to /people with PersonData as JSON body
+        | [<EndPoint "PUT /people"; Json "personData">]
+            EditPerson of personData: PersonData
+
+        /// Accepts DELETE requests to /people/{id}
+        | [<EndPoint "DELETE /people">]
+            DeletePerson of id: int
+
+    /// The type of all endpoints for the application.
+    type EndPoint =
+        
+        /// Accepts requests to /
+        | [<EndPoint "/">] Home
+
+        /// Accepts requests to /api/...
+        | [<EndPoint "/api">] Api of Cors<ApiEndPoint>
+
+    /// Error result value.
+    type Error = { error : string }
+
+    /// Alias representing the success or failure of an operation.
+    /// The Ok case contains a success value to return as JSON.
+    /// The Error case contains an HTTP status and a JSON error to return.
+    type ApiResult<'T> = Result<'T, Http.Status * Error>
+
+    /// Result value for CreatePerson.
+    type Id = { id : int }
+
+open Model
+
+
+module Backend =
+
+    /// The people database.
+    /// This is a dummy implementation, of course; a real-world application
+    /// would go to an actual database.
+    let private people = new Dictionary<int, PersonData>()
+
+    /// The highest id used so far, incremented each time a person is POSTed.
+    let private lastId = ref 0
+
+    let personNotFound() : ApiResult<'T> =
+        Error (Http.Status.NotFound, { error = "Person not found." })
+
+    let GetPeople () : ApiResult<PersonData[]> =
+        lock people <| fun () ->
+            people
+            |> Seq.map (fun (KeyValue(_, person)) -> person)
+            |> Array.ofSeq
+            |> Ok
+
+    let GetPerson (id: int) : ApiResult<PersonData> =
+        lock people <| fun () ->
+            match people.TryGetValue(id) with
+            | true, person -> Ok person
+            | false, _ -> personNotFound()
+
+    let CreatePerson (data: PersonData) : ApiResult<Id> =
+        lock people <| fun () ->
+            incr lastId
+            people.[!lastId] <- { data with id = !lastId }
+            Ok { id = !lastId }
+
+    let EditPerson (data: PersonData) : ApiResult<Id> =
+        lock people <| fun () ->
+            match people.TryGetValue(data.id) with
+            | true, _ ->
+                people.[data.id] <- data
+                Ok { id = data.id }
+            | false, _ -> personNotFound()
+
+    let DeletePerson (id: int) : ApiResult<Id> =
+        lock people <| fun () ->
+            match people.TryGetValue(id) with
+            | true, _ ->
+                people.Remove(id) |> ignore
+                Ok { id = id }
+            | false, _ -> personNotFound()
+
+    // On application startup, pre-fill the database with a few people.
+    do List.iter (CreatePerson >> ignore) [
+        { id = 0
+          firstName = "Alonzo"
+          lastName = "Church"
+          born = DateTime(1903, 6, 14)
+          died = Some(DateTime(1995, 8, 11)) }
+        { id = 0
+          firstName = "Alan"
+          lastName = "Turing"
+          born = DateTime(1912, 6, 23)
+          died = Some(DateTime(1954, 6, 7)) }
+        { id = 0
+          firstName = "Bertrand"
+          lastName = "Russell"
+          born = DateTime(1872, 5, 18)
+          died = Some(DateTime(1970, 2, 2)) }
+        { id = 0
+          firstName = "Noam"
+          lastName = "Chomsky"
+          born = DateTime(1928, 12, 7)
+          died = None }
+    ]
 module Site =
     open WebSharper.UI.Html
 
-    let HomePage ctx =
-        Templating.Main ctx EndPoint.Home "Home" [
-            h1 [] [text "Say Hi to the server!"]
-            div [] [client <@ Client.Main() @>]
-        ]
+        /// Helper function to convert our internal ApiResult type into WebSharper Content.
+    let JsonContent (result: ApiResult<'T>) : Async<Content<EndPoint>> =
+        match result with
+        | Ok value ->
+            Content.Json value
+        | Error (status, error) ->
+            Content.Json error
+            |> Content.SetStatus status
+        |> Content.WithContentType "application/json"
 
-    let AboutPage ctx =
-        Templating.Main ctx EndPoint.About "About" [
-            h1 [] [text "About"]
-            p [] [text "This is a template WebSharper client-server application."]
-        ]
+    /// Respond to an ApiEndPoint by calling the corresponding backend function
+    /// and converting the result into Content.
+    let ApiContent (ep: ApiEndPoint) : Async<Content<EndPoint>> =
+        match ep with
+        | GetPeople ->
+            JsonContent (Backend.GetPeople ())
+        | GetPerson id ->
+            JsonContent (Backend.GetPerson id)
+        | CreatePerson personData ->
+            JsonContent (Backend.CreatePerson personData)
+        | EditPerson personData ->
+            JsonContent (Backend.EditPerson personData)
+        | DeletePerson id ->
+            JsonContent (Backend.DeletePerson id)
+
+    let HomePage ctx =
+        // let person1Link = ctx.Link ("/api/people/1")
+        Content.Page(
+            Body = [
+                p [] [text "API is running."]
+                p [] [
+                    text "Try querying: "
+                    a [attr.href "/api/people/1"] [text "/api/people/1"]
+                ]
+            ]
+        )
 
     [<Website>]
-    let Main =
+    let Main corsAllowedOrigins =
         Application.MultiPage (fun ctx endpoint ->
             match endpoint with
             | EndPoint.Home -> HomePage ctx
-            | EndPoint.About -> AboutPage ctx
+            | Api api ->
+                Content.Cors api (fun allows ->
+                    { allows with
+                        Origins = corsAllowedOrigins
+                        Headers = ["Content-Type"]
+                    }
+                ) ApiContent
         )
--- project42/TwitterAPI/Startup.fs
+++ project42/TwitterAPI/Startup.fs
@@ -10,19 +10,30 @@ open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
 open WebSharper.AspNetCore
 
+type Website(config: IConfiguration) =
+    inherit SiteletService<Model.EndPoint>()
+
+    let corsAllowedOrigins =
+        config.GetSection("allowedOrigins").AsEnumerable()
+        |> Seq.map (fun kv -> kv.Value)
+        |> List.ofSeq
+
+    override val Sitelet = Site.Main corsAllowedOrigins
+    // override val Sitelet = Site.Main
+
 type Startup() =
 
     member this.ConfigureServices(services: IServiceCollection) =
-        services.AddSitelet(Site.Main)
-            .AddAuthentication("WebSharper")
-            .AddCookie("WebSharper", fun options -> ())
-        |> ignore
+        services.AddSitelet<Website>() |> ignore
+        // services.AddSitelet(Site.Main) |> ignore
+            // .AddAuthentication("WebSharper")
+            // .AddCookie("WebSharper", fun options -> ())
+        // |> ignore
 
     member this.Configure(app: IApplicationBuilder, env: IWebHostEnvironment) =
         if env.IsDevelopment() then app.UseDeveloperExceptionPage() |> ignore
 
-        app.UseAuthentication()
-            .UseStaticFiles()
+        app.UseStaticFiles()
             .UseWebSharper()
             .Run(fun context ->
                 context.Response.StatusCode <- 404
